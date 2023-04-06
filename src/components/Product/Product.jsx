bc115e6dd1974d919b9e6a6ebe1e4b345dd9094f
Alireza <alirezasha01@gmail.com>
2023-04-06 22:20:43 +0430

    change product component to class component for practice

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 
-const Product = () =>{
-    return(
+// const Product = () =>{
+//     return(
+//         <div>
+//             <h1>Product</h1>
+//         </div>
+//     )
+
+// }
+
+class Product extends React.Component{
+    render(){
+        return(
         <div>
             <h1>Product</h1>
         </div>
-    )
-
+        )
+    }
 }
 
 export default Product

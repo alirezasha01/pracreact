1c120e65368f2b8929751fdeef950e07db016f98
Alireza <alirezasha01@gmail.com>
2023-04-07 14:28:53 +0430

    practicing props in functional component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Product from "./components/Product/Product";
 const App = () => {
   return (
     <div className="App">
-      <Product/>
+      <Product name="shampoo" price="100000"/>
+      <Product name="ketab" price="500000"/>
+      <Product name="samavar" price="2900000"/>
       <h1>test</h1>
     </div>
   );
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,24 +1,12 @@
 import React from "react";
 
-// const Product = () =>{
-//     return(
-//         <div>
-//             <h1>Product</h1>
-//         </div>
-//     )
-
-// }
-
-class Product extends React.Component{
-    render(){
-        const price = 10
-        return(
-        <div>
-            <p>product Name: shampoo</p>
-            <p>product Price: {price}</p>
-        </div>
-        )
-    }
+const Product = (props) =>{
+    return(
+    <div>
+        <p>product Name: {props.name}</p>
+        <p>product Price: {props.price}</p>
+    </div>
+    )
 }
 
 export default Product
